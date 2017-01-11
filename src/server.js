a23d859f6a6bb8b2800ca813c20c4039030fc929
uzaif nilger <uzaif313@gmail.com>
2017-01-11 22:20:18 +0530

    Add route handler with GET method and 404

--- src/server.js
+++ src/server.js
@@ -1,10 +1,46 @@
 
-'use strict'
-
+'use strict';
+    
     const http = require('http')
+    const url = require('url')
+    
+    let routes ={
+        'GET':{
+            '/':(req,res)=>{
+                res.writeHead(200,{'Content-Type':'text/html'})
+                res.end("<h1>Home Page</h1>");
+            },
+            '/api/getData':(req,res)=>{
+                res.writeHead(200,{'Content-Type':'application/json'})
+                res.end(JSON.stringify(req.data))
+             },
+             '/about':(req,res)=>{
+                 res.writeHead(200,{'Content-Type':'text/html'})
+                 res.end("<h1>We are at About Page</h1>")
+             }
+        },
+        'POST':{
 
-    http.
-        createServer((req,res)=>{
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.end("<h1> Fire with node</h1>");
-        }).listen(3000,()=>{console.log("Server is running on Port 3000")})
+        },
+        '404':(req,res)=>{
+            res.writeHead(404)
+            res.end("It look like you are at wrong place");
+        }
+    }
+
+    function router(req,res){
+        let baseURL = url.parse(req.url,true)
+        let routeResolver = routes[req.method][baseURL.pathname]
+
+        if (routeResolver != undefined){
+            req.data = baseURL.query
+            routeResolver(req,res)
+        }
+        else{
+            routes['404'](req,res) 
+        }
+    }
+
+    http.createServer(router).listen(3000,()=>{
+        console.log("Server Runnning on Port 3000");
+    })
